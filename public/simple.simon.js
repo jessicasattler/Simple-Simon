494b85cf65504a1723c7408ebc44d7b0a567759a
Jessica Sattler <jessicasattler28@gmail.com>
2016-08-17 21:52:25 -0500

    Basic html and css layout of Simple Simon boxes. Added function to make boxes have a dull color when "start" button is clicked.

---
+++ public/simple.simon.js
@@ -0,0 +1,12 @@
+(function(){
+'use strict';
+	
+	var listenerStartButton =function(event){
+		$(".button").css("opacity","0.85");
+	};
+
+	$("#start").click(listenerStartButton);
+	//gives dullness to boxes at the click of "Start" button
+
+
+})();
